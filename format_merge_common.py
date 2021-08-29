790c56ca8b1a5060edd135de5a3c812695a9c816
jimman2003 <jimman2003@hotmail.com>
2021-08-29 20:52:57 +0300

    make format_merge use the system arguments

--- format_merge_common.py
+++ format_merge_common.py
@@ -1,5 +1,6 @@
 import re
+import sys 
 def match_records(type_string):
-    start_string=input("enter string with olids: ")   
+    start_string=sys.argv[1]   
     return [match.group(0) for match in re.finditer(rf"OL\d+{type_string}",start_string)]
 
