96cbb775bcaa76252476aaa9fdd7789ee464d4fd
jimman2003 <jimman2003@hotmail.com>
2021-08-30 11:41:10 +0300

    handle multiple args in format_merge_common

--- format_merge_common.py
+++ format_merge_common.py
@@ -1,6 +1,7 @@
 import re
 import sys 
 def match_records(type_string):
-    start_string=sys.argv[1]   
-    return [match.group(0) for match in re.finditer(rf"OL\d+{type_string}",start_string)]
+    args=sys.argv[1:]
+    return [match.group(0) for arg in args for match in re.finditer(rf"OL\d+{type_string}",arg)] 
+     
 
